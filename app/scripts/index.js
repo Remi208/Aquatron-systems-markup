aed80ce944c9580053256eb5a47d153d57c9cb76
remi208 <s208@tmm-sapr.org>
2018-05-19 11:47:16 +0300

    header and 2 blocks ready

--- app/scripts/index.js
+++ app/scripts/index.js
@@ -1,3 +1,35 @@
 import "./markup-menu";
+var $ = require("jquery");
 
-document.addEventListener("DOMContentLoaded", () => {});
+document.addEventListener("DOMContentLoaded", () => {
+    popup();
+    menu();
+});
+
+function popup() {
+    $('.btn-callback').click( function(event){
+        event.preventDefault();
+        $('.overlay').fadeIn(400,
+            function(){
+                $('.callback-popup')
+                    .css('display', 'block')
+                    .animate({opacity: 1}, 200);
+            });
+    });
+    $('.popup-close, .overlay').click( function(){
+        $('.callback-popup')
+            .animate({opacity: 0}, 200,
+                function(){
+                    $(this).css('display', 'none');
+                    $('.overlay').fadeOut(400);
+                }
+            );
+    });
+}
+
+function menu(){
+    $('.nav-icon').click(function(){
+        $(this).toggleClass('open');
+        $(".main-nav").toggleClass('active');
+    });
+}
